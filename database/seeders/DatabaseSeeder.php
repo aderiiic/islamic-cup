1303cd60311ca1ee7af7649d8488694785bc6628
amarderic <amar.deric@webbi.se>
2025-11-08 17:28:36 +0100

    DB Seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,26 +16,36 @@ class DatabaseSeeder extends Seeder
         // Admin-konto
         if (!User::where('email', 'admin@islamiccup.se')->exists()) {
             User::create([
-                'name' => 'Admin',
-                'email' => 'admin@islamiccup.se',
-                'password' => Hash::make('password'), // byt i produktion
+                'name' => 'Walid',
+                'email' => 'W_schallala@hotmail.com',
+                'password' => Hash::make('Bismillah123'), // byt i produktion
                 'role' => 'admin',
                 'phone' => '+46123456789',
+                'organization' => 'Islamic Cup',
+                'can_create_multiple_teams' => true,
+            ]);
+
+            User::create([
+                'name' => 'Amar',
+                'email' => 'amar.deric@webbi.se',
+                'password' => Hash::make('Bismillah123'), // byt i produktion
+                'role' => 'admin',
+                'phone' => '+46736103918',
                 'organization' => 'Islamic Cup',
                 'can_create_multiple_teams' => true,
             ]);
         }
 
         // Exempel-moderator (valfritt)
-        if (!User::where('email', 'moderator@islamiccup.se')->exists()) {
-            User::create([
-                'name' => 'Moderator',
-                'email' => 'moderator@islamiccup.se',
-                'password' => Hash::make('password'),
-                'role' => 'moderator',
-                'organization' => 'Islamic Cup',
-            ]);
-        }
+//        if (!User::where('email', 'moderator@islamiccup.se')->exists()) {
+//            User::create([
+//                'name' => 'Moderator',
+//                'email' => 'moderator@islamiccup.se',
+//                'password' => Hash::make('password'),
+//                'role' => 'moderator',
+//                'organization' => 'Islamic Cup',
+//            ]);
+//        }
 
         // Ta bort fabriksexemplet om du inte vill skapa en test-user
         // User::factory()->create([
